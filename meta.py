71400af7e6a2320d6d58f311e107c75e51b084e6
ColdRiver <17830831606@163.com>
2025-12-24 04:51:30 +0800

    2025122405

--- meta.py
+++ meta.py
@@ -2,15 +2,35 @@ from jinja2 import Environment, FileSystemLoader
 from markdown import markdown
 from ex import *
 from pprint import pp
+from typing import List
 
 env = Environment(loader=FileSystemLoader('templates'))
 template = env.get_template('code.py.tpl')
 
+def ex_question(raw:str) -> List[str]:
+    for line in raw.split("\n"):
+        if line == "": continue
+        length = len(line)
+        positions = [0]
+        formula = False
+        for i,c in enumerate(line):
+            if "$" == c: 
+                formula = not formula
+                continue
+            if formula: continue
+            print(c,end="")
+        # for i,pos in enumerate(positions):
+        #     if i == 0: p = line[:pos]
+        #     elif i == length: p = line[pos:]
+        #     else: p = line[i:i+1]
+        #     print(p)
 def generate_code(scripts:str):   
     lines = []
     top_levels = extract_top_level_tags_in_order(scripts)
     for top_level in top_levels:
-        print(top_level["tag"])
+        if top_level["tag"] == "question":
+            ex_question(top_level["content"])
+                
     result = template.render(lines=lines)
     
     with open("./result.py","w",encoding="utf-8") as f:
